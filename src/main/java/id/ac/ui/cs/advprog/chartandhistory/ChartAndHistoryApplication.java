45a4763a0efa83ea7d12e7d103a17dfd3b77723b
ilhamzik <zikriilham2@gmail.com>
2024-04-17 19:27:01 +0700

    init

---
+++ src/main/java/id/ac/ui/cs/advprog/chartandhistory/ChartAndHistoryApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.chartandhistory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChartAndHistoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChartAndHistoryApplication.class, args);
+    }
+
+}
