23d601f356fe94fc0c2c91fc79b8410816dc9b2e
ilhamzik <zikriilham2@gmail.com>
2024-05-15 14:15:59 +0700

    adjust some package structure of my application

--- src/main/java/ChartAndHistory/CartAndHistoryApplication.java
+++ src/main/java/ChartAndHistory/CartAndHistoryApplication.java
@@ -1,13 +1,13 @@
-package id.ac.ui.cs.advprog.chartandhistory;
+package ChartAndHistory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChartAndHistoryApplication {
+public class CartAndHistoryApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ChartAndHistoryApplication.class, args);
+        SpringApplication.run(CartAndHistoryApplication.class, args);
     }
 
 }
