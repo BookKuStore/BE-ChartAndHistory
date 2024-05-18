af06e52fc44476b261c92b6fd9edcdd2b2a5bbb6
ilhamzik <zikriilham2@gmail.com>
2024-05-18 18:38:11 +0700

    [RED] Create HistoryRepositoryTest

---
+++ src/main/java/ChartAndHistory/backend/repository/HistoryRepository.java
@@ -0,0 +1,8 @@
+package ChartAndHistory.backend.repository;
+
+import ChartAndHistory.backend.models.History;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface HistoryRepository extends JpaRepository<History, Long> {
+}
