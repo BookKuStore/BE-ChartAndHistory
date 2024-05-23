fe4bc25f6c958e3918ccd27254a7dbc2a7529ebd
ilhamzik <zikriilham2@gmail.com>
2024-05-23 21:46:39 +0700

    Deployment trial 2

--- src/test/java/ChartAndHistory/CartAndHistoryApplicationTests.java
+++ src/test/java/ChartAndHistory/CartAndHistoryApplicationTests.java
@@ -1,9 +1,16 @@
 package ChartAndHistory;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 
-@SpringBootTest
+@ActiveProfiles("test")
+@DataJpaTest
+@ContextConfiguration
+//@SpringBootTest
 class CartAndHistoryApplicationTests {
 
     @Test
--- src/test/java/ChartAndHistory/backend/repository/CartRepositoryTest.java
+++ src/test/java/ChartAndHistory/backend/repository/CartRepositoryTest.java
@@ -6,15 +6,20 @@ import ChartAndHistory.backend.repository.CartRepository;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
+
+@ActiveProfiles("test")
+//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 
 @DataJpaTest
 public class CartRepositoryTest {
